234e7966fc80f97f3edbb21513ab81fb0e4a8229
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-10-15 00:21:01 +0600

    add Navbar bootstrap

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div></div>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Full Stack CURD Application</a>
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button> */}
+                    <button className='btn btn-outline-light'>Add User</button>
+                </div>
+            </nav>
+        </div>
     )
 }
 
