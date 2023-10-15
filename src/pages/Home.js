9bf96b46f5f181dfcf5a5120e382d9f563b8d03e
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-10-16 00:39:09 +0600

    delete user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
+    const { id } = useParams()
+    const deleteUser = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`);
+        loadUsers();
+    }
 
     const loadUsers = async () => {
         const result = await axios.get("http://localhost:8080/users");
@@ -37,7 +42,7 @@ const Home = () => {
                                     <td>
                                         <button className='btn btn-primary mx-2'>View</button>
                                         <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
-                                        <button className='btn btn-danger mx-2'>Delete</button>
+                                        <button className='btn btn-danger mx-2' onClick={() => deleteUser(user.id)}>Delete</button>
                                     </td>
                                 </tr>
                             ))
