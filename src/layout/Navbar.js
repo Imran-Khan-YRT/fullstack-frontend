8eaea502fb001be9540ab1669cd5055dd578f9f2
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-10-16 01:18:58 +0600

    view user section

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,10 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Full Stack CURD Application</a>
-                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button> */}
+                    <Link className="navbar-brand" to={"/"}>Full Stack CURD Application</Link>
                     <Link className='btn btn-outline-light' to="/adduser">Add User</Link>
                 </div>
             </nav>
